fc1dbe3279360ec0f80c9dabea420a97704d6698
Celine <cmdev@outlook.fr>
2026-02-18 18:09:21 +0100

    Update ssr server

--- frontend/server.ts
+++ frontend/server.ts
@@ -83,7 +83,7 @@ app.use('*all', async (req, res) => {
       render = (await vite.ssrLoadModule('/src/entry-server.ts')).render
     } else {
       template = templateHtml
-      render = (await import('./dist/server/entry-server.mjs')).render
+      render = (await import('./dist/server/entry-server.js')).render
     }
 
     const rendered = await render(url)
